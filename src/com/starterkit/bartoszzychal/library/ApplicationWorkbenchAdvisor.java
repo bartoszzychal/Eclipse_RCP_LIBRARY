a80fcd49ee4e5b783455c67a1d611b1e464d9592
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-04-15 08:32:21 +0200

    init

---
+++ src/com/starterkit/bartoszzychal/library/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package com.starterkit.bartoszzychal.library;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "Library.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
