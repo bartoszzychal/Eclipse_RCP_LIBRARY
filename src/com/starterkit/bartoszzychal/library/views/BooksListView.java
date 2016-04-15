caabc3385b1526c1b4049c8f6ffe0ba5e7c39434
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-04-15 11:57:54 +0200

    working table

--- src/com/starterkit/bartoszzychal/library/views/BooksListView.java
+++ src/com/starterkit/bartoszzychal/library/views/BooksListView.java
@@ -1,79 +1,119 @@
 package com.starterkit.bartoszzychal.library.views;
 
+import java.util.logging.Logger;
+
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.MouseEvent;
-import org.eclipse.swt.events.MouseListener;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
 
+import com.starterkit.bartoszzychal.library.dataProvider.DataProvider;
 import com.starterkit.bartoszzychal.library.dataProvider.data.Book;
-import com.starterkit.bartoszzychal.restConnection.RestConnection;
+import com.starterkit.bartoszzychal.library.model.LibraryModel;
 
 public class BooksListView extends ViewPart {
 
-	private RestConnection restConnect;
-	
+	private final Logger LOG = Logger.getLogger(getClass().getSimpleName());
+	private final DataProvider dataProvider = new DataProvider();
+	private final LibraryModel libraryModel = new LibraryModel(dataProvider);
+	private TableViewer viewer;
+
 	public BooksListView() {
-		restConnect = new RestConnection();
 	}
 
 	@Override
 	public void createPartControl(Composite parent) {
-		parent.setLayout(new GridLayout(1, false));
+		GridLayout layout = new GridLayout(1, false);
+		parent.setLayout(layout);
 		
-		Button btnAddNewBook = new Button(parent, SWT.NONE);
-		btnAddNewBook.addSelectionListener(new SelectionAdapter() {
+		Label searchTitleLabel = new Label(parent, SWT.NONE);
+		searchTitleLabel.setText("Search by title: ");
+		final Text searchTitleText = new Text(parent, SWT.BORDER | SWT.SEARCH);
+		searchTitleText.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL | GridData.HORIZONTAL_ALIGN_FILL));
+		
+		Label searchBookLabel = new Label(parent, SWT.NONE);
+		searchBookLabel.setText("Search by author: ");
+		final Text searchAuthorText = new Text(parent, SWT.BORDER | SWT.SEARCH);
+		searchAuthorText.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL | GridData.HORIZONTAL_ALIGN_FILL));
+		
+		createViewer(parent);
+	}
+
+	private void createViewer(Composite parent) {
+		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
+		
+		createColumns(parent, viewer);
+		
+		final Table table = viewer.getTable();
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+
+		viewer.setContentProvider(new ArrayContentProvider());
+		libraryModel.update();
+		viewer.setInput(libraryModel.getBooks());
+		// make the selection available to other views
+		getSite().setSelectionProvider(viewer);
+
+		// define layout for the viewer
+		GridData gridData = new GridData();
+		gridData.verticalAlignment = GridData.FILL;
+		gridData.horizontalSpan = 2;
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.grabExcessVerticalSpace = true;
+		gridData.horizontalAlignment = GridData.FILL;
+		viewer.getControl().setLayoutData(gridData);
+	}
+
+	private void createColumns(final Composite parent, final TableViewer viewer) {
+		String[] titles = { "ID", "TITLE", "AUTHORS" };
+		int[] bounds = { 50, 100, 100};
+
+		TableViewerColumn col = createTableViewerColumn(titles[0], bounds[0], 0);
+		col.setLabelProvider(new ColumnLabelProvider() {
 			@Override
-			public void widgetSelected(SelectionEvent e) {
+			public String getText(Object element) {
+				Book book = (Book) element;
+				return Long.toString(book.getId());
 			}
 		});
-		btnAddNewBook.setText("ADD NEW BOOK");
-		btnAddNewBook.addMouseListener(new MouseListener() {
-			
+
+		col = createTableViewerColumn(titles[1], bounds[1], 1);
+		col.setLabelProvider(new ColumnLabelProvider() {
 			@Override
-			public void mouseUp(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseDown(MouseEvent e) {
-				restConnect.addNewBook(new Book(0, "title", "author"));			
-			}
-			
-			@Override
-			public void mouseDoubleClick(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
+			public String getText(Object element) {
+				Book book = (Book) element;
+				return book.getTitle();
 			}
 		});
-		
-		Button btnFindAllBooks = new Button(parent, SWT.NONE);
-		btnFindAllBooks.setText("FIND ALL BOOKS");
-		
-		btnFindAllBooks.addMouseListener(new MouseListener() {
-			
+
+		col = createTableViewerColumn(titles[2], bounds[2], 2);
+		col.setLabelProvider(new ColumnLabelProvider() {
 			@Override
-			public void mouseUp(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseDown(MouseEvent e) {
-				restConnect.getBooks();
-			}
-			
-			@Override
-			public void mouseDoubleClick(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
+			public String getText(Object element) {
+				Book book = (Book) element;
+				return book.getAuthors();
 			}
 		});
+
+	}
+
+	private TableViewerColumn createTableViewerColumn(String title, int bound, final int colNumber) {
+		final TableViewerColumn viewerColumn = new TableViewerColumn(viewer, SWT.NONE);
+		final TableColumn column = viewerColumn.getColumn();
+		column.setText(title);
+		column.setWidth(bound);
+		column.setResizable(true);
+		column.setMoveable(true);
+		return viewerColumn;
 	}
 
 	@Override
@@ -81,5 +121,4 @@ public class BooksListView extends ViewPart {
 		// TODO Auto-generated method stub
 
 	}
-
 }
