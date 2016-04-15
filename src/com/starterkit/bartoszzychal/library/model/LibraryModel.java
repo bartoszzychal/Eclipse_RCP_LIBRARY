f453b1b532c7cd9a31ccb7aa45f18cc10a4021f7
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-04-15 11:58:21 +0200

    Created model

--- src/com/starterkit/bartoszzychal/library/model/LibraryModel.java
+++ src/com/starterkit/bartoszzychal/library/model/LibraryModel.java
@@ -1,5 +1,28 @@
 package com.starterkit.bartoszzychal.library.model;
 
-public class LibraryModel {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.starterkit.bartoszzychal.library.dataProvider.DataProvider;
+import com.starterkit.bartoszzychal.library.dataProvider.data.Book;
+
+public class LibraryModel {
+	
+	private DataProvider dataProvider;
+	private List<Book> books = new ArrayList<>();
+
+	public LibraryModel(DataProvider dataProvider){
+		this.dataProvider = dataProvider;
+	}
+	
+	public List<Book> getBooks(){
+		return books;
+	}
+	
+	public void update(){
+		books.clear();
+		books.addAll(dataProvider.allBook());
+	}
+	
+	
 }
